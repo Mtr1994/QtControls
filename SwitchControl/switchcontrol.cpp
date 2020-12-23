63c9f926f28ff4916f8aa55d0b66161b4169f897
Mtr1994 <36178164+Mtr1994@users.noreply.github.com>
2020-12-23 14:57:24 +0800

    Add files via upload

---
+++ SwitchControl/switchcontrol.cpp
@@ -0,0 +1,151 @@
+﻿#include "switchcontrol.h"
+
+#include <QPainter>
+#include <QPen>
+#include <QPainterPath>
+#include <QSize>
+
+using namespace Mtr;
+
+SwitchControl::SwitchControl(QWidget *parent) : QWidget(parent)
+{
+    mTimeline.setDuration(240);
+    mTimeline.setEasingCurve(QEasingCurve::InSine);
+    connect(&mTimeline, &QTimeLine::frameChanged, this, &SwitchControl::slot_current_frame_change);
+}
+
+void SwitchControl::SwitchOn()
+{
+    if (mTimeline.state() == QTimeLine::Running) return;
+    mTimeline.setFrameRange(0, 100);
+    mTimeline.start();
+}
+
+void SwitchControl::SwitchOff()
+{
+    if (mTimeline.state() == QTimeLine::Running) return;
+    mTimeline.setFrameRange(100, 0);
+    mTimeline.start();
+}
+
+bool SwitchControl::isSwitchOn()
+{
+    return mTimeline.currentFrame() == 100;
+}
+
+void SwitchControl::paintEvent(QPaintEvent *event)
+{
+    QPainter painter(this);
+    painter.setRenderHint(QPainter::Antialiasing);
+
+    QPen pen;
+    pen.setCapStyle(Qt::RoundCap);
+    pen.setJoinStyle(Qt::RoundJoin);
+    pen.setWidth(1);
+    pen.setColor(QColor(mBorderColor));
+    painter.setPen(pen);
+
+    float width = this->width();
+    float height = this->height();
+
+    int frame = mTimeline.currentFrame();
+
+    QPainterPath path;
+    float radiusBackground = (height - 8) * 0.5;
+    path.addRoundedRect(QRectF(4, 4, width - 8, height - 8), radiusBackground, radiusBackground);
+    painter.fillPath(path, (frame < 50) ? QBrush(QColor(mBackgroundOffColor)) : QBrush(QColor(mBackgroundOnColor)));
+    painter.drawRoundedRect(QRectF(4, 4, width - 8, height - 8), radiusBackground, radiusBackground);
+
+    QPainterPath dotPath;
+    float radiusDot = (height - 14) * 0.5;
+    float max = (width - height * 2);
+    float add = frame == 100 ? 0 : (frame > 50) ? (100 - frame) / 50.0 * max : frame / 50.0 * max;
+    add = add < 0 ? 0 : add;
+    dotPath.addRoundedRect(QRectF(7 + (frame / 100.00) * (width - height), 7, (height - 14) + add, height - 14), radiusDot, radiusDot);
+    painter.fillPath(dotPath, QBrush(QColor(mDotColor)));
+
+    event->accept();
+}
+
+void SwitchControl::enterEvent(QEvent *event)
+{
+    mEntered = true;
+    event->accept();
+}
+
+void SwitchControl::leaveEvent(QEvent *event)
+{
+    mEntered = false;
+    event->accept();
+}
+
+void SwitchControl::mousePressEvent(QMouseEvent *event)
+{
+    mPressed = true;
+    if (event->button() != Qt::LeftButton) return;
+    event->accept();
+}
+
+void SwitchControl::mouseReleaseEvent(QMouseEvent *event)
+{
+    if (!mEntered || !mPressed) return;
+    mPressed = false;
+    event->accept();
+    if (mTimeline.state() == QTimeLine::Running) return;
+    if (mTimeline.currentFrame() == 100) mTimeline.setFrameRange(100, 0);
+    else mTimeline.setFrameRange(0, 100);
+    mTimeline.start();
+}
+
+void SwitchControl::resizeEvent(QResizeEvent *event)
+{
+    setMinimumHeight(48);
+    setMinimumWidth(height() * 2.2);
+    event->accept();
+}
+
+void SwitchControl::slot_current_frame_change(int frame)
+{
+    Q_UNUSED(frame);
+    update();
+}
+
+QString SwitchControl::getBorderColor() const
+{
+    return mBorderColor;
+}
+
+void SwitchControl::setBorderColor(const QString &borderColor)
+{
+    mBorderColor = borderColor;
+}
+
+QString SwitchControl::getDotColor() const
+{
+    return mDotColor;
+}
+
+void SwitchControl::setDotColor(const QString &dotColor)
+{
+    mDotColor = dotColor;
+}
+
+QString SwitchControl::getBackgroundOffColor() const
+{
+    return mBackgroundOffColor;
+}
+
+void SwitchControl::setBackgroundOffColor(const QString &backgroundOffColor)
+{
+    mBackgroundOffColor = backgroundOffColor;
+}
+
+QString SwitchControl::getBackgroundOnColor() const
+{
+    return mBackgroundOnColor;
+}
+
+void SwitchControl::setBackgroundOnColor(const QString &backgroundOnColor)
+{
+    mBackgroundOnColor = backgroundOnColor;
+}
---
+++ SwitchControl/switchcontrol.h
@@ -0,0 +1,64 @@
+﻿#ifndef SWITCHCONTROL_H
+#define SWITCHCONTROL_H
+
+#include <QWidget>
+#include <QPaintEvent>
+#include <QMouseEvent>
+#include <QTimeLine>
+
+namespace Mtr {
+
+class SwitchControl : public QWidget
+{
+    Q_OBJECT
+public:
+    explicit SwitchControl(QWidget *parent = nullptr);
+
+    void SwitchOn();
+    void SwitchOff();
+
+    bool isSwitchOn();
+
+    QString getBackgroundOnColor() const;
+    void setBackgroundOnColor(const QString &backgroundOnColor);
+
+    QString getBackgroundOffColor() const;
+    void setBackgroundOffColor(const QString &backgroundOffColor);
+
+    QString getDotColor() const;
+    void setDotColor(const QString &dotColor);
+
+    QString getBorderColor() const;
+    void setBorderColor(const QString &borderColor);
+
+protected:
+    void paintEvent(QPaintEvent *event) override;
+
+    void enterEvent(QEvent *event) override;
+
+    void leaveEvent(QEvent *event) override;
+
+    void mousePressEvent(QMouseEvent *event) override;
+
+    void mouseReleaseEvent(QMouseEvent *event) override;
+
+    void resizeEvent(QResizeEvent *event) override;
+
+private slots:
+    void slot_current_frame_change(int frame);
+
+private:
+    QString mBackgroundOnColor      = "#5BC560";
+    QString mBackgroundOffColor     = "#DDDDDD";
+    QString mDotColor               = "#FEFEFE";
+    QString mBorderColor            = "#CCCCCC";
+
+    QTimeLine mTimeline;
+
+    bool mEntered = false;
+    bool mPressed = false;
+};
+
+}
+
+#endif // SWITCHCONTROL_H
